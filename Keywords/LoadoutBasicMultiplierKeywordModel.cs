c25ff04287dc738beb6fe51169579389596f047e
qw2341 <>
2026-08-29 22:03:17 +0800

    Added Increase and Decrease for multiplier keywords

--- Keywords/LoadoutBasicMultiplierKeywordModel.cs
+++ Keywords/LoadoutBasicMultiplierKeywordModel.cs
@@ -2,11 +2,13 @@
 
 namespace Loadout.Keywords;
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Loadout.Services.TildeKey;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 
@@ -19,6 +21,11 @@ public enum LoadoutBasicMultiplierTarget
 public abstract class LoadoutBasicMultiplierKeywordModel
     : LoadoutBasicKeywordModel
 {
+    public const string DirectionVarName =
+        "LoadoutBasicMultiplierDirection";
+    public const string DisplayAmountVarName =
+        "LoadoutBasicMultiplierDisplayAmount";
+
     private IReadOnlyList<LoadoutKeywordDynamicVarDefinition>? _dynamicVars;
 
     public abstract string PercentageVarName { get; }
@@ -41,6 +48,22 @@ public abstract class LoadoutBasicMultiplierKeywordModel
                 "DYNAMIC_VAR_LOADOUT_BASIC_MULTIPLIER_PERCENTAGE",
                 (name, value) => new IntVar(name, value))
         ];
+
+    protected override void AddCardTextVariables(
+        CardModel card,
+        LocString cardText)
+    {
+        DynamicVar amount = GetAmount(card, PercentageVarName);
+        string directionKey = amount.BaseValue < 0m
+            ? "LOADOUT-BASIC_MULTIPLIER_DIRECTION_DECREASE"
+            : "LOADOUT-BASIC_MULTIPLIER_DIRECTION_INCREASE";
+        cardText.Add(
+            DirectionVarName,
+            new LocString("card_keywords", directionKey));
+        cardText.Add(new DynamicVar(
+            DisplayAmountVarName,
+            Math.Abs(amount.BaseValue)));
+    }
 
     public override Task AfterOnPlay(
         CardModel card,
