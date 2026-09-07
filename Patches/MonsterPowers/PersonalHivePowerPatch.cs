3c39950292a77637120fa71c5bd96241b0365e20
qw2341 <>
2026-09-07 14:13:47 +0800

    Made the personal hive power work for players morphed as Entomancer

--- Patches/MonsterPowers/PersonalHivePowerPatch.cs
+++ Patches/MonsterPowers/PersonalHivePowerPatch.cs
@@ -4,8 +4,12 @@ namespace Loadout.Patches.MonsterPowers;
 
 using System.Threading.Tasks;
 using HarmonyLib;
+using Loadout.Services.Morphing;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Monsters;
 using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
 
 [HarmonyPatch(typeof(PersonalHivePower), nameof(PersonalHivePower.AfterDamageReceived))]
 public static class PersonalHivePowerPatch
@@ -15,6 +19,7 @@ public static class PersonalHivePowerPatch
         PersonalHivePower __instance,
         Creature target,
         Creature? dealer,
+        ValueProp props,
         ref Task __result)
     {
         if (!__instance.Owner.IsPlayer
@@ -25,7 +30,23 @@ public static class PersonalHivePowerPatch
             return true;
         }
 
-        __result = Task.CompletedTask;
+        __result = props.IsPoweredAttack()
+                   && dealer.IsMonster
+                   && dealer.Side != __instance.Owner.Side
+                   && BottledMonsterMorphService.IsPlayerMorphedAs<Entomancer>(
+                       __instance.Owner.Player)
+            ? StunForTurns(dealer, __instance.Amount)
+            : Task.CompletedTask;
         return false;
+    }
+
+    private static Task StunForTurns(Creature monster, int turns)
+    {
+        if (turns <= 0 || monster.IsDead)
+            return Task.CompletedTask;
+
+        return CreatureCmd.Stun(
+            monster,
+            _ => StunForTurns(monster, turns - 1));
     }
 }
